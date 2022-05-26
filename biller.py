b33db5cd3c9c7b4dfbedce9234590492359ada93
Abdul Shan <77062434+AbdulShan@users.noreply.github.com>
2022-05-27 01:31:13 +0530

    Update biller.py

--- biller.py
+++ biller.py
@@ -1,3 +1,13 @@
 from tkinter import *
 
-if "__name__"==__main__
+
+root=Tk()
+#window=root.Tk()
+width= root.winfo_screenwidth()
+height= root.winfo_screenheight()
+root.geometry("{}x{}".format(width, height))
+
+root.title("Billing App")
+root.configure(bg="#18181b")
+
+root.mainloop()
