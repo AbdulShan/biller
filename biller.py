1221f2d4c005ceec63fe9a1009f96a08a9d9eba9
Abdul Shan <77062434+AbdulShan@users.noreply.github.com>
2022-06-12 13:58:18 +0530

    Update biller.py

--- biller.py
+++ biller.py
@@ -20,16 +20,13 @@ if "__main__"==__name__:
     root.geometry("%dx%d" % (width, height))
     root.resizable(False,False)
     root.state('zoomed')
-    
-    
-
-    
+      
 
 def top_frame():  
-    
     ###############################################################################
     top_frame = LabelFrame(root, bg="white",fg="white")
     top_frame.grid(row=0, column=0,sticky="w")
+    
     
     #Mobile
     customer_number_lbl=Label(top_frame,text="Customer Number",font=book_antiqua)
@@ -57,10 +54,11 @@ def top_frame():
     
     bill_number_tb=Entry(top_frame,font=arial)
     bill_number_tb.grid(row=1,column=2)
+    
 
     
 
-    #Button Save
+    '''#Button Save
     save_btn=Button(top_frame,text="Enter",padx=10,pady=5,command=lambda:[create_data()])
     save_btn.grid(row=1,column=3,padx=(0, 368))
 
@@ -72,7 +70,7 @@ def top_frame():
             cur.execute("create table if not exists '{}'(bill_number int PRIMARY KEY NOT NULL,date varchar(15),customer_name varchar(30),sl_no int,product_name varchar(100),quantity int,unit_rate int,price int)".format(customer_number))
             con.close()
         except sqlite3.Error as err:
-            print("Error - ",err)
+            print("Error - ",err)'''
 
     ################################################################################
     mid_frame = LabelFrame(root, bg="white",fg="white")
@@ -120,49 +118,44 @@ def top_frame():
     total_amount_tb=Entry(mid_frame,font=arial)
     total_amount_tb.grid(row=1,column=4)
 
+    #Submit
+    enter=Button(mid_frame,text="Enter",padx=10,pady=5,command=lambda:[clear_all(),fetch_data(),display()])
+    enter.grid(row=1,column=5)
     
 
-    #Submit
-    enter=Button(mid_frame,text="Enter",padx=10,pady=5,command=lambda:[fetch_data(),display()])
-    enter.grid(row=1,column=5)
-
     def fetch_data():
+
+        
+        product_name=product_name_tb.get()
+        customer_number=customer_number_tb.get()
+        customer_name=customer_name_tb.get()
+        bill_number=bill_number_tb.get()
+        
+        quantity_ins=quantity_tb.get()
+        unit_rate_ins=unit_rate_tb.get()
+        total_amount=total_amount_tb.get()
+        
+
+        '''product_id_tb.delete('0', END)
+        product_name_tb.delete('0', END)
+        quantity_tb.delete('0', END)
+        unit_rate_tb.delete('0', END)
+        total_amount_tb.delete('0', END)'''
+
+        
         try:
             con=sqlite3.connect("Customer_Data.sql")
             cur=con.cursor()
             num=1
             customer_number=customer_number_tb.get()
-            customer_name=customer_name_tb.get()
-            bill_number=bill_number_tb.get()
-            product_name=product_name_tb.get()
-            quantity_ins=quantity_tb.get()
-            unit_rate_ins=unit_rate_tb.get()
-            total_amount=total_amount_tb.get()
-            cur.execute("create table if not exists '{}'(bill_number int PRIMARY KEY NOT NULL, date varchar(15), customer_name varchar(30), sl_no int, product_name varchar(100), quantity int, unit_rate int, price int)".format(customer_number))
-            cur.execute("INSERT or IGNORE into '{}'(bill_number,date,customer_name,sl_no,product_name,quantity,unit_rate,price)VALUES('{}','{}','{}','{}','{}','{}','{}','{}')".format(customer_number,bill_number,datesorted,customer_name,num,product_name,quantity_ins,unit_rate_ins,total_amount))
+            cur.execute("create table if not exists '{}'(productname varchar PRIMARY KEY NOT NULL,date varchar(15),customer_name varchar(30),sl_no int,bill_num varchar(100),quantity int,unit_rate int,price int)".format(customer_number))
+            cur.execute("INSERT into '{}'(productname,date,customer_name,sl_no,bill_num,quantity,unit_rate,price)VALUES('{}','{}','{}',{},{},{},{},{})".format(customer_number,product_name,datesorted,customer_name,num,bill_number,quantity_ins,unit_rate_ins,total_amount))
             con.commit()
             con.close()
         except sqlite3.Error as err:
             print("Error - ",err)
-            '''print('SQLite error: %s' % (' '.join(err.args)))
-            print("Exception class is: ", err.__class__)
-            print('SQLite traceback: ')
-            exc_type, exc_value, exc_tb = sys.exc_info()
-            print(traceback.format_exception(exc_type, exc_value, exc_tb))'''
 
-    def display():
-        try:
-            con=sqlite3.connect('Customer_Data.sql')
-            cur=con.cursor()
-            customer_number=customer_number_tb.get()
-            cur.execute("SELECT * from '{}'".format(customer_number))
-            rec=cur.fetchall()
-            for i in rec:
-                print("   ",i[0],"  |",i[1],"   |",i[2],"   |",i[3],"   ",i[4],"  |",i[5],"   |",i[6],"   |",i[7],"   ")
-            con.commit()
-            con.close()
-        except sqlite3.Error as err:
-            print("Error- ",err)
+        
 
 
     ##############################################################################
@@ -180,7 +173,7 @@ def top_frame():
     tree_view.configure(xscrollcommand=vertical_scrollbar.set)
 
     #Definning number of columns
-    tree_view["columns"]=("1","2","3","4","5")
+    tree_view["columns"]=("1","2","3","4","5","6","7","8")
     #defining heading
     tree_view["show"]='headings'
     #modifying the size of the columns
@@ -189,16 +182,39 @@ def top_frame():
     tree_view.column("3",width=90)
     tree_view.column("4",width=90)
     tree_view.column("5",width=150)
+    tree_view.column("6",width=150)
+    tree_view.column("7",width=150)
+    tree_view.column("8",width=150)
     #assigning heading name
-    tree_view.heading("1",text="SL no.")
-    tree_view.heading("2",text="Product Name")
-    tree_view.heading("3",text="Quantity")
-    tree_view.heading("4",text="Unit Rate")
-    tree_view.heading("5",text="Price")
+    tree_view.heading("1",text="Bill Number")
+    tree_view.heading("2",text="Date")
+    tree_view.heading("3",text="Customer Name")
+    tree_view.heading("4",text="SL no.")
+    tree_view.heading("5",text="Product Name")
+    tree_view.heading("6",text="Quantity")
+    tree_view.heading("7",text="Unit Rate")
+    tree_view.heading("8",text="Price")
+    def display():
+        try:
+            con=sqlite3.connect('Customer_Data.sql')
+            cur=con.cursor()
+            customer_number=customer_number_tb.get()
+            cur.execute("SELECT * from '{}'".format(customer_number))
+            rec=cur.fetchall()
+            count=cur.rowcount
+            for i in rec:
+                print("   ",i[0],"  |",i[1],"   |",i[2],"   |",i[3],"   ",i[4],"  |",i[5],"   |",i[6],"   |",i[7],"   ")
+                tree_view.insert("", 'end', text ="L1",values =(i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7]))
+            con.commit()
+            con.close()
+        except sqlite3.Error as err:
+            print("Error- ",err)
+    
+    def clear_all():
+        for item in tree_view.get_children():
+            tree_view.delete(item)
 
+        
 
-
-
-                
 top_frame()
 root.mainloop()
