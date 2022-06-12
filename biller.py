a48a1fafe82511474b1a79d2c664f8b807b638a4
Abdul Shan <77062434+AbdulShan@users.noreply.github.com>
2022-06-12 22:05:24 +0530

    Update biller.py

--- biller.py
+++ biller.py
@@ -1,9 +1,14 @@
+from __future__ import print_function
 import datetime
+from logging import PlaceHolder
 from tkinter import *
 import sqlite3
 import traceback
 import sys
 from tkinter.ttk import Treeview
+import atexit
+from os import path
+from json import dumps, loads
 
 book_antiqua=("Book Antiqua",12,"bold")
 arial=('Arial', 12)
@@ -11,7 +16,21 @@ arial=('Arial', 12)
 date=datetime.date.today()
 datesorted=date.strftime("%d-%m-%Y")
 
+#Bill Number Counter
 
+def read_counter():
+    return loads(open("counter.json", "r").read()) + 1 if path.exists("counter.json") else 0
+
+
+def write_counter():
+    with open("counter.json", "w") as f:
+        f.write(dumps(counter))
+
+
+counter = read_counter()
+atexit.register(write_counter)
+
+#
 if "__main__"==__name__:
     root=Tk()
     root.title("Billing App")
@@ -20,7 +39,6 @@ if "__main__"==__name__:
     root.geometry("%dx%d" % (width, height))
     root.resizable(False,False)
     root.state('zoomed')
-      
 
 def top_frame():  
     ###############################################################################
@@ -35,8 +53,8 @@ def top_frame():
     customer_number_tb=Entry(top_frame,font=arial)
     customer_number_tb.grid(row=1,column=0)
 
-    customer_number_tb.insert(0,"_")
-    customer_number_tb.focus_set()
+    '''customer_number_tb.insert(0,"_")
+    customer_number_tb.focus_set()'''
     
 
     #Name
@@ -53,10 +71,10 @@ def top_frame():
     bill_number_lbl.grid(row=0,column=2)
     
     bill_number_tb=Entry(top_frame,font=arial)
+    bill_number_tb.insert(0,counter)
+    bill_number_tb.config(state='disabled')
     bill_number_tb.grid(row=1,column=2)
-    
 
-    
 
     '''#Button Save
     save_btn=Button(top_frame,text="Enter",padx=10,pady=5,command=lambda:[create_data()])
@@ -129,11 +147,13 @@ def top_frame():
         product_name=product_name_tb.get()
         customer_number=customer_number_tb.get()
         customer_name=customer_name_tb.get()
-        bill_number=bill_number_tb.get()
-        
+        bill_number=counter
         quantity_ins=quantity_tb.get()
         unit_rate_ins=unit_rate_tb.get()
-        total_amount=total_amount_tb.get()
+        
+        total_amount_tb.delete(0,END)
+        total_amount=int(quantity_ins)*int(unit_rate_ins)
+        total_amount_tb.insert(0,total_amount)
         
 
         '''product_id_tb.delete('0', END)
@@ -142,11 +162,11 @@ def top_frame():
         unit_rate_tb.delete('0', END)
         total_amount_tb.delete('0', END)'''
 
-        
+        num=1
         try:
             con=sqlite3.connect("Customer_Data.sql")
             cur=con.cursor()
-            num=1
+            
             customer_number=customer_number_tb.get()
             cur.execute("create table if not exists '{}'(productname varchar PRIMARY KEY NOT NULL,date varchar(15),customer_name varchar(30),sl_no int,bill_num varchar(100),quantity int,unit_rate int,price int)".format(customer_number))
             cur.execute("INSERT into '{}'(productname,date,customer_name,sl_no,bill_num,quantity,unit_rate,price)VALUES('{}','{}','{}',{},{},{},{},{})".format(customer_number,product_name,datesorted,customer_name,num,bill_number,quantity_ins,unit_rate_ins,total_amount))
@@ -173,7 +193,7 @@ def top_frame():
     tree_view.configure(xscrollcommand=vertical_scrollbar.set)
 
     #Definning number of columns
-    tree_view["columns"]=("1","2","3","4","5","6","7","8")
+    tree_view["columns"]=("1","2","3","4","5")
     #defining heading
     tree_view["show"]='headings'
     #modifying the size of the columns
@@ -182,18 +202,12 @@ def top_frame():
     tree_view.column("3",width=90)
     tree_view.column("4",width=90)
     tree_view.column("5",width=150)
-    tree_view.column("6",width=150)
-    tree_view.column("7",width=150)
-    tree_view.column("8",width=150)
     #assigning heading name
-    tree_view.heading("1",text="Bill Number")
-    tree_view.heading("2",text="Date")
-    tree_view.heading("3",text="Customer Name")
-    tree_view.heading("4",text="SL no.")
-    tree_view.heading("5",text="Product Name")
-    tree_view.heading("6",text="Quantity")
-    tree_view.heading("7",text="Unit Rate")
-    tree_view.heading("8",text="Price")
+    tree_view.heading("1",text="Sl.No")
+    tree_view.heading("2",text="Product Name")
+    tree_view.heading("3",text="Quantity")
+    tree_view.heading("4",text="Unit Rate")
+    tree_view.heading("5",text="Price")
     def display():
         try:
             con=sqlite3.connect('Customer_Data.sql')
@@ -203,8 +217,8 @@ def top_frame():
             rec=cur.fetchall()
             count=cur.rowcount
             for i in rec:
-                print("   ",i[0],"  |",i[1],"   |",i[2],"   |",i[3],"   ",i[4],"  |",i[5],"   |",i[6],"   |",i[7],"   ")
-                tree_view.insert("", 'end', text ="L1",values =(i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7]))
+                print("   ",i[3],"  |",i[0],"   |",i[5],"   ",i[6],"  |",i[7],"   |")
+                tree_view.insert("", 'end', text ="L1",values =(i[3],i[0],i[5],i[6],i[7]))
             con.commit()
             con.close()
         except sqlite3.Error as err:
